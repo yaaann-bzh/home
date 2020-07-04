14ab59fefdf63264dbd839fc2f9e11f703447ab2
yann <yann-tachier@hotmail.fr>
2020-07-04 15:45:23 +0200

    Error 404 page

--- src/components/errors/404.vue
+++ src/components/errors/404.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-      <h1>Erreur 404</h1>
-  </div>
+	<div>
+		<h1>Erreur 404 - Page non trouvée</h1>
+		<p class="lead">
+			La page à laquelle vous tentez d'accéder est introuvable. <br>
+			Vous pouvez contacter l'administrateur du site si vous pensez qu'il s'agit d'un bug.
+		</p>
+	</div>
 </template>
 
 <script>
