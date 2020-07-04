c07d4de90b00c4bb8eb5518843aac04c5a0c7272
yann <yann-tachier@hotmail.fr>
2020-07-04 16:35:27 +0200

    Main routes transition

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
 		<app-header></app-header>
 		<div class="container pt-md-5 pb-5" id="main-container">
 			<div class="row">
-				<router-view class="col-12 col-md-10 offset-md-1"></router-view>
+				<transition name="slide" type="out-in">
+					<router-view class="col-12 col-md-10 offset-md-1"></router-view>
+				</transition>
 			</div>
 		</div>
 		<app-footer class="row position-absolute w-100"></app-footer>
@@ -49,6 +51,37 @@ footer {
 
 hr{
 	background-color: #28a745 !important;
+}
+
+.slide-enter-active {
+	animation: slide-in 300ms ease-out forwards;
+}
+
+.slide-leave-active {
+	animation: slide-out 300ms ease-out forwards;
+	position: absolute;
+}
+
+@keyframes slide-in {
+	from {
+		transform: translateX(-100%);
+		opacity: 0;
+	}
+	to {
+		transform: translateX(0);
+		opacity: 1;
+	}
+}
+
+@keyframes slide-out {
+	from {
+		transform: translateX(0);
+		opacity: 1;
+	}
+	to {
+		transform: translateX(100%);
+		opacity: 0;
+	}
 }
 
 </style>
